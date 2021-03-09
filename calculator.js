be5ae0d3f377e8c13ef0ee86bb2eb83775053770
Heranca <heranca.kamalo@gmail.com>
2021-03-09 14:29:44 +0100

    operator

--- calculator.js
+++ calculator.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+
+
+let firstValue = prompt("value 1 : ");
+let operator = prompt("operator : ");
+let secondValue = prompt("value 2 : ");
+
+console.log(firstValue);
+console.log(operator);
+console.log(secondValue);
